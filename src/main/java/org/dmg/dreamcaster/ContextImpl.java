040bec416efcffd3b232db3613355ba2a6010834
litanontaru <Ghtvmthf5%t>
2020-11-09 12:43:09 +0100

    added mapping

--- src/main/java/org/dmg/dreamcaster/ContextImpl.java
+++ src/main/java/org/dmg/dreamcaster/ContextImpl.java
@@ -1,5 +1,7 @@
 package org.dmg.dreamcaster;
 
+import static java.util.function.Function.identity;
+
 import lombok.RequiredArgsConstructor;
 import org.dmg.dreamcaster.model.Activation;
 import org.dmg.dreamcaster.model.Effect;
@@ -7,13 +9,23 @@ import org.dmg.dreamcaster.model.Formula;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ContextImpl implements Context {
     private final List<Formula> formulas;
+
+    private Map<String, Formula> mapped;
+
+    @PostConstruct
+    private void init() {
+        mapped = formulas.stream().collect(Collectors.toMap(Formula::getName, identity()));
+    }
 
     @Override
     public Effect createEffect(String name) {
@@ -27,10 +39,6 @@ public class ContextImpl implements Context {
 
     @Override
     public Function<int[], Integer> formula(String name) {
-        return formulas
-                .stream()
-                .filter(f -> f.getName().equals(name))
-                .findFirst()
-                .get();
+        return mapped.get(name);
     }
 }
