278763155e6cf961c6ab678248cfc5b5125c9517
litanontaru <Ghtvmthf5%t>
2020-11-13 13:31:25 +0100

    split yml file

--- src/main/java/org/dmg/dreamcaster/editor/paser/Main.java
+++ src/main/java/org/dmg/dreamcaster/editor/paser/Main.java
@@ -1,19 +1,29 @@
 package org.dmg.dreamcaster.editor.paser;
 
+import static java.util.stream.Collectors.toList;
+
 import org.dmg.dreamcaster.editor.yaml.Elements;
 import org.dmg.dreamcaster.editor.yaml.PowerList;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
+    private static List<String> files = Arrays.asList(
+            "elements",
+            "aspects",
+            "damage"
+    );
+
     public static void main(String[] args) {
-        Elements elements;
-        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("examples/elements.yml")) {
-            elements = new Yaml().loadAs(inputStream, Elements.class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        List<Elements> elementsList = files.stream().map(Main::getElements).collect(toList());
+        Elements elements = new Elements();
+        elements.setElements(elementsList.stream().map(Elements::getElements).filter(Objects::nonNull).flatMap(Collection::stream).collect(toList()));
+        elements.setAspects(elementsList.stream().map(Elements::getAspects).filter(Objects::nonNull).flatMap(Collection::stream).collect(toList()));
 
         PowerList powers;
         try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("examples/powers.yml")) {
@@ -29,5 +39,15 @@ public class Main {
         }
 
         System.out.println();
+    }
+
+    private static Elements getElements(String filename) {
+        Elements elements;
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("examples/" + filename + ".yml")) {
+            elements = new Yaml().loadAs(inputStream, Elements.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return elements;
     }
 }
